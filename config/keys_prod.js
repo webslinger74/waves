3486d021e025b8300eebafc1923415c90713c7e8
webslinger74 <webster_steven@yahoo.co.uk>
2018-09-16 10:04:34 -0700

    setup

---
+++ config/keys_prod.js
@@ -0,0 +1,10 @@
+module.exports = {
+    mongoURI: process.env.MONGO_URI,
+    secretOrKey: process.env.SECRET_OR_KEY
+}
+
+
+
+
+
+ 
---
+++ server.js
@@ -0,0 +1,57 @@
+const express  = require('express');
+const path = require('path');
+// const open = require('open');
+const bodyParser = require('body-parser');
+// const webpack = require('webpack');
+const mongoose = require('mongoose');
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+const passport = require('passport');
+
+
+const db = require('./config/keys.js').mongoURI;
+
+//connect to MongoDB
+
+mongoose.connect(db, { server: { auto_reconnect: true } }, { useNewUrlParser: true })
+    .then(()=> console.log('db connected'))
+    .catch((error) => console.log(error));
+
+
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+//passport middleware
+app.use(passport.initialize());
+
+//passport config
+
+require('./config/passport')(passport);
+
+//user routes
+
+app.use('/api/users', users);
+
+
+app.listen(port, (error)=> {
+    if(error){
+        console.log(error);
+    }  else {
+       console.log("server runnning")
+   }
+})
+
+//Server static assets if in production
+
+if(process.env.NODE_ENV === "production"){
+app.use(express.static('client/build'));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+})
+}
+
