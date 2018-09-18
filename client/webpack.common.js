7932478aa7395edd8a8895a1092105e08dd5530e
webslinger74 <webster_steven@yahoo.co.uk>
2018-09-18 10:46:47 -0700

    added some client files

---
+++ client/webpack.common.js
@@ -0,0 +1,50 @@
+var webpack = require('webpack');
+var path = require('path');
+var htmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+    entry: ["babel-polyfill", path.join(__dirname, './src/index.js')],
+    output:{
+        path: path.join(__dirname, 'build'),
+        filename: 'bundle.js',
+        publicPath:'/'
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+        template:'index.html',
+      filename:'index.html',
+    inject: 'body'
+    })
+   ], 
+  
+   module: {
+    rules: [
+        {
+            test: /.js?$/,
+            loader:'babel-loader',
+            exclude: /node_modules/
+        },
+        {
+           test: /\.css$/,
+                    use: [
+                       'style-loader',
+                       'css-loader',
+                       'resolve-url-loader'
+                     ]
+                   },
+                    {
+                    test: /\.(png|jpg|gif)$/,
+                    use: [
+                      {
+                        loader:'url-loader',
+                        options: {
+                          
+                        }
+                      }
+                    ]
+                  }       
+    ]
+}     
+    
+}
---
+++ client/webpack.config.dev.js
@@ -0,0 +1,34 @@
+var webpack = require('webpack');
+var path = require('path');
+const merge = require('webpack-merge');
+const common = require('./webpack.common');
+
+
+
+
+module.exports = merge(common, {
+    mode:'development',
+   devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './build',
+        port:8080,
+        historyApiFallback:true,   //this ensures the site goes back to root to load react router from home page
+        proxy: {
+            "/api/users/register" :{
+                target:"http://localhost:5000", 
+                secure:"false" 
+             },
+            "/api/**/**" :{
+                target:"http://localhost:5000",
+                secure:"false"
+            },
+            "/api/profile/handle/**" : {
+                target:"http://localhost:5000",
+                secure:"false"
+            }
+           
+          }
+
+    }
+   
+})
