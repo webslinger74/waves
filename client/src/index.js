3880c5f1a2878db8d71515229df2b1887d795484
webslinger74 <webster_steven@yahoo.co.uk>
2018-09-19 15:29:32 -0700

    redux set up

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './routes';
 import '../public/resources/css/styles.css';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import { combineReducers, compose} from 'redux';
+//import authReducer from '../src/reducers/authReducer';
+import errorsReducer from '../src/reducers/errorsReducer';
+import userReducer from '../src/reducers/user_reducer';
+
+const initialState = {};
+const rootReducers = combineReducers({
+        auth: userReducer,
+        errors: errorsReducer
+})
+
+const middleware = [thunk];
+
+const store = createStore(
+                        rootReducers,
+                        initialState,
+                        compose(
+                            applyMiddleware(...middleware),
+                            window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+                        )
+                        );
+
 
 ReactDOM.render(
-        <BrowserRouter>
-        <Routes />
-        </BrowserRouter>
-
+        <Provider store={ store }>
+                 <BrowserRouter>
+                         <Routes />
+                 </BrowserRouter>
+        </Provider>
 , document.getElementById('root'));
