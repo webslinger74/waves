1a883d1e0b9e4a81191ba6a0bb1f4aa02a7b8b10
webslinger74 <webster_steven@yahoo.co.uk>
2018-09-25 16:23:28 -0700

    guitar checkboxes

--- client/src/components/shop/Shop.js
+++ client/src/components/shop/Shop.js
@@ -3,35 +3,64 @@ import PageTop from '../../utils/PageTop';
 import { connect } from 'react-redux';
 import { getBrands, getWoods } from '../../actions/product_actions';
 
-
+import CollapseCheckBox from '../Inputs/CollapseCheckBox';
 
 class Shop extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
-    }
 
+    state = {
+        filters:{
+            brands:[],
+            frets:[],
+            price:[],
+            woods:[]
+        }
+    }
+  
     componentDidMount(){
         this.props.getBrands();
         this.props.getWoods();
     }
 
+    handleFilters = (filters,title) => {
+        const newFilters = {...this.state.filters}
 
+        newFilters[title] = filters
+
+            this.setState({
+                filters: newFilters
+            })
+
+        console.log(this.state, "state of shop")
+    }
 
     render() { 
-
-        const { products } = this.props;
-
+            
+        const { brands, woods } = this.props;
+            console.log(brands, "this is brandy")
+            
         return (
+
+
             <div>
-
-
                 <PageTop  title="BrowseProducts" />
 
                 <div className="container">
                     <div className="shop_wrapper">
                         <div className="left">
-                            left
+                            <CollapseCheckBox 
+                            initState={true}
+                            title="brands"
+                            names={brands}  
+                            handleFilters={(filters) => 
+                                this.handleFilters(filters, 'brands')}
+                            />
+                             <CollapseCheckBox 
+                            initState={true}
+                            title="woods"
+                            names={woods}  
+                            handleFilters={(filters) => 
+                                this.handleFilters(filters, 'woods')}
+                            />
                         </div>
                         <div className="right">
                             Right
@@ -59,7 +88,9 @@ const actions = {
 }
 
 const mapStateToProps = (state) => ({
-    products: state.props
+    products: state.products,
+    brands: state.products.brands,
+    woods:state.products.woods
 })
  
 export default connect(mapStateToProps, actions)(Shop);
